40789482452df42b46587398f90cf710d374a759
Olivier MICHAUD <olivier@micoli.org>
2019-10-06 23:41:06 +0200

    Add an email grouping feature

--- scripts/send.js
+++ scripts/send.js
@@ -84,6 +84,17 @@ var messages = [
         cid: 'image'
       }
     ]
+  },
+
+  // Another email with custom group header
+  {
+    from: 'FatMancho <fat@mancho.com>',
+    to: 'Jason Patric <jason.patric@times.com>',
+    subject: 'Hells Kitchen',
+    headers: {
+      'X-maildev-group': 'group1'
+    },
+    html: 'You want a Rolls-Royce, you don\'t come here, no no. You go to England, or wherever the .... they make it.'
   }
 
 ]
